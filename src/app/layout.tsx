d023f83ddd2ab46ff4578c3c5664ade9efffbe61
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-12-09 19:47:31 -0300

    Criando Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/header";
 
 
 export const metadata: Metadata = {
@@ -14,9 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <>
+        <body>
+          <Header/>
+          {children}
+        </body>
+      </>
     </html>
   );
 }
