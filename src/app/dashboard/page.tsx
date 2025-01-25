b7af85f6a031d96157c31d4da338edaf0a3071d2
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-01-24 22:05:59 -0300

    Criando Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,43 @@
-import styles from './styles.module.css';
-import Head from 'next/head';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import styles from "./styles.module.css";
+import Head from "next/head";
+import { Textarea } from "@/components/textarea";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    // Obtendo a sessão no lado do servidor
+    const session = await getServerSession(authOptions);
+
+    // Redireciona para a página inicial se o usuário não estiver logado
+    if (!session) {
+        redirect("/");
+    }
+
     return (
         <div className={styles.container}>
             <Head>
                 <title>Painel de Tarefas</title>
             </Head>
 
-            <h1>Painel de Tarefas</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+                        <form action="">
+                            <Textarea 
+                            placeholder="Digite sua tarefa aqui..."/>
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox} />
+                                <label>Deixar tarefa pública?</label>
+                            </div>
+                            <button className={styles.button} type="submit">Registrar</button>
+                        </form>
+                    </div>
+                </section>
+            </main>
+            
         </div>
     );
 }
