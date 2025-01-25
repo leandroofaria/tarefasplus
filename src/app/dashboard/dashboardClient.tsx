e3dc09ae23e2ff3201332fd08ce78633d611985b
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-01-25 02:26:56 -0300

    Listando Itens

--- src/app/dashboard/dashboardClient.tsx
+++ src/app/dashboard/dashboardClient.tsx
@@ -5,13 +5,51 @@ import Head from "next/head";
 import { Textarea } from "@/components/textarea";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import { db } from "@/services/firebaseConnection";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, query, orderBy, where, onSnapshot } from "firebase/firestore";
+
+interface TaskProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  tarefa: string;
+  user: string
+}
 
 export default function DashboardClient({ session }: { session: any }) {
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
+  const [tasks, setTasks] = useState<TaskProps[]>([])
+
+  useEffect(() => { 
+    async function loadTarefas() {
+      const tarefasRef = collection(db, "tarefas");
+      const q = query(
+        tarefasRef,
+        orderBy("created", "desc"),
+        where("user", "==", session.user.email)
+      );
+
+      onSnapshot(q, (snapshot) => {
+        let lista = [] as TaskProps[]
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            tarefa: doc.data().tarefa,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public,
+          })
+        })
+
+        setTasks(lista);
+      });
+    }
+    loadTarefas();
+}, [session.user.email]);
+
 
   function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
     setPublicTask(event.target.checked);
@@ -32,7 +70,6 @@ export default function DashboardClient({ session }: { session: any }) {
 
       setInput("");
       setPublicTask(false);
-      alert("Tarefa registrada com sucesso!");
     } catch (erro) {
       console.log(erro);
     }
@@ -75,22 +112,27 @@ export default function DashboardClient({ session }: { session: any }) {
 
         <section className={styles.taskContainer}>
           <h1>Minhas Tarefas</h1>
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
+          
+          {tasks.map((item) => (
+            <article key={item.id} className={styles.task}>
+            {item.public && (
+              <div className={styles.tagContainer}>
               <label className={styles.tag}>Publico</label>
               <button className={styles.shareButton}>
                 <FiShare2 size={22} color="#3183ff" />
               </button>
             </div>
+            )}
 
             <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo</p>
+              <p>{item.tarefa}</p>
 
               <button className={styles.trashButton}>
                 <FaTrash size={24} color="#ea3140" />
               </button>
             </div>
           </article>
+          ))}
         </section>
       </main>
     </div>
