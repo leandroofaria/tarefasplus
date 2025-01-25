389e25ae4a4b9483898fd5f21517d992944a659b
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-01-24 23:12:52 -0300

    Finalizando Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import styles from "./styles.module.css";
 import Head from "next/head";
 import { Textarea } from "@/components/textarea";
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
 
 export default async function Dashboard() {
     // Obtendo a sessão no lado do servidor
@@ -35,6 +37,26 @@ export default async function Dashboard() {
                             <button className={styles.button} type="submit">Registrar</button>
                         </form>
                     </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas Tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>Publico</label>
+                            <button className={styles.shareButton}>
+                                <FiShare2 size={22} color="#3183ff"/>
+                            </button>
+                        </div>
+
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa de exemplo</p>
+
+                            <button className={styles.trashButton}>
+                                <FaTrash size={24} color="#ea3140"/>
+                            </button>
+                        </div>
+                    </article>
                 </section>
             </main>
             
