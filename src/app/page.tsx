186c2c6e75a9f3190415c6a2682dab664187536a
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-01-27 19:04:52 -0300

    Criando Home Estática

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import Image from 'next/image';
 import heroImg from '../../public/assets/hero.png';
 import styles from '../styles/home.module.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/services/firebaseConnection';
 
+export const revalidate = 60; // Revalida a cada 60 segundos
 
-export default function Home() {
+export default async function Home() {
+  // Carrega os dados diretamente no componente
+  const commentRef = collection(db, "comentarios");
+  const postRef = collection(db, "tarefas");
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  const posts = postSnapshot.size || 0; // Obtenha o tamanho corretamente
+  const comments = commentSnapshot.size || 0;
+
   return (
-    
     <div className={styles.container}>
       <main className={styles.main}>
-
         <div className={styles.logoContent}>
           <Image 
             className={styles.hero} 
@@ -21,13 +31,12 @@ export default function Home() {
         
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
-
       </main>
     </div>
   );
